0034c3f3d28dfa27b050d594703d64410ddde4eb
Srihari S <srihari.sundaramurugan@gmail.com>
2023-06-12 00:42:32 +0530

    Added tracking and file attachment

---
+++ routes/emailRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const emailController = require("../controllers/emailController");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+
+router.post(
+	"/send-email",
+	upload.fields([
+		{ name: "file", maxCount: 1 },
+		{ name: "attachment", maxCount: 1 },
+	]),
+	emailController.sendEmails
+);
+router.get("/track/:uuid", emailController.trackEmail);
+router.get("/email-stats", emailController.getEmailStats);
+
+module.exports = router;
